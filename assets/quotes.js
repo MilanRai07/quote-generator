b2a420d958003e140f45d614d41d4a1b62c6f597
milanrai <milanraiyt07@gmail.com>
2024-09-14 11:14:15 +0545

    project setup2, added js file, img file and asset file with quotes data

---
+++ assets/quotes.js
@@ -0,0 +1,152 @@
+let quotes = [
+  {
+    "id": "1",
+    "quote": "The only way to do great work is to love what you do.",
+    "category": "inspiration",
+    "author": "Steve Jobs"
+  },
+  {
+    "id": "2",
+    "quote": "Life is what happens when you're busy making other plans.",
+    "category": "life",
+    "author": "John Lennon"
+  },
+  {
+    "id": "3",
+    "quote": "The purpose of our lives is to be happy.",
+    "category": "happiness",
+    "author": "Dalai Lama"
+  },
+  {
+    "id": "4",
+    "quote": "Get busy living or get busy dying.",
+    "category": "motivational",
+    "author": "Stephen King"
+  },
+  {
+    "id": "5",
+    "quote": "You only live once, but if you do it right, once is enough.",
+    "category": "life",
+    "author": "Mae West"
+  },
+  {
+    "id": "6",
+    "quote": "In three words I can sum up everything I've learned about life: it goes on.",
+    "category": "life",
+    "author": "Robert Frost"
+  },
+  {
+    "id": "7",
+    "quote": "The best way to predict your future is to create it.",
+    "category": "inspiration",
+    "author": "Peter Drucker"
+  },
+  {
+    "id": "8",
+    "quote": "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    "category": "self",
+    "author": "Ralph Waldo Emerson"
+  },
+  {
+    "id": "9",
+    "quote": "You miss 100% of the shots you don’t take.",
+    "category": "motivational",
+    "author": "Wayne Gretzky"
+  },
+  {
+    "id": "10",
+    "quote": "Happiness is not something ready made. It comes from your own actions.",
+    "category": "happiness",
+    "author": "Dalai Lama"
+  },
+  {
+    "id": "11",
+    "quote": "Success is not the key to happiness. Happiness is the key to success.",
+    "category": "success",
+    "author": "Albert Schweitzer"
+  },
+  {
+    "id": "12",
+    "quote": "To be great is to be misunderstood.",
+    "category": "inspiration",
+    "author": "Ralph Waldo Emerson"
+  },
+  {
+    "id": "13",
+    "quote": "The only limit to our realization of tomorrow is our doubts of today.",
+    "category": "inspiration",
+    "author": "Franklin D. Roosevelt"
+  },
+  {
+    "id": "14",
+    "quote": "Believe you can and you're halfway there.",
+    "category": "motivational",
+    "author": "Theodore Roosevelt"
+  },
+  {
+    "id": "15",
+    "quote": "Act as if what you do makes a difference. It does.",
+    "category": "motivational",
+    "author": "William James"
+  },
+  {
+    "id": "16",
+    "quote": "The only way to achieve the impossible is to believe it is possible.",
+    "category": "inspiration",
+    "author": "Charles Kingsleigh"
+  },
+  {
+    "id": "17",
+    "quote": "What lies behind us and what lies before us are tiny matters compared to what lies within us.",
+    "category": "inspiration",
+    "author": "Ralph Waldo Emerson"
+  },
+  {
+    "id": "18",
+    "quote": "Do not wait to strike till the iron is hot, but make it hot by striking.",
+    "category": "motivational",
+    "author": "William Butler Yeats"
+  },
+  {
+    "id": "19",
+    "quote": "The only person you are destined to become is the person you decide to be.",
+    "category": "self",
+    "author": "Ralph Waldo Emerson"
+  },
+  {
+    "id": "20",
+    "quote": "Everything you’ve ever wanted is on the other side of fear.",
+    "category": "motivational",
+    "author": "George Addair"
+  },
+  {
+    "id": "21",
+    "quote": "The journey of a thousand miles begins with one step.",
+    "category": "motivational",
+    "author": "Lao Tzu"
+  },
+  {
+    "id": "22",
+    "quote": "Your time is limited, so don’t waste it living someone else’s life.",
+    "category": "inspiration",
+    "author": "Steve Jobs"
+  },
+  {
+    "id": "23",
+    "quote": "To succeed in life, you need two things: ignorance and confidence.",
+    "category": "humor",
+    "author": "Mark Twain"
+  },
+  {
+    "id": "24",
+    "quote": "It does not matter how slowly you go as long as you do not stop.",
+    "category": "motivational",
+    "author": "Confucius"
+  },
+  {
+    "id": "25",
+    "quote": "We may encounter many defeats but we must not be defeated.",
+    "category": "inspiration",
+    "author": "Maya Angelou"
+  }
+]
